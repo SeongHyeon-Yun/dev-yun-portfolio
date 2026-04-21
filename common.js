7cdab3c2a41e5c2fefa9c09519e99c9e823fa528
yun seong hyeon <oko9495@naver.com>
2026-04-21 21:51:00 +0900

    포트폴리오 페이지 제작

--- common.js
+++ common.js
@@ -2,3 +2,29 @@ document.addEventListener("mousemove", (e) => {
   document.body.style.setProperty("--x", e.clientX + "px");
   document.body.style.setProperty("--y", e.clientY + "px");
 });
+
+const get_audio = document.getElementById("get-audio");
+
+get_audio.addEventListener("click", () => {
+  const div = document.createElement("div");
+  const close_btn = document.createElement("div");
+  const audio_con = document.createElement("div");
+
+  div.id = "audio-area";
+  audio_con.id = "audio-con";
+  close_btn.classList.add("close_btn");
+  close_btn.innerText = "닫기";
+
+  const audio = document.createElement("video");
+
+  audio.src = "/img/odd_ing.mp4"; // 경로 or URL
+  audio.controls = true; // 재생 버튼 보이게
+  audio_con.appendChild(audio);
+  document.body.appendChild(div);
+  div.appendChild(close_btn);
+  div.appendChild(audio_con);
+
+  close_btn.addEventListener("click", () => {
+    div.remove();
+  });
+});
